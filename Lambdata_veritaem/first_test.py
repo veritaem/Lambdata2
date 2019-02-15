8fa0f01a9879a0cd17f49827bea5d4faec5a4792
veritaem <dcharris776@gmail.com>
2019-02-14 19:04:29 -0500

    yay added dockerfile and test case #1

---
+++ Lambdata_veritaem/first_test.py
@@ -0,0 +1,7 @@
+import unittest
+from first import three_way_split
+
+class test_suite(unittest.TestCase):
+    '''tests to ensure proper three way splitting of function three_way_split'''    def test_split(self):
+        self.assert X_val, X_test, X_train, y_test, y_train, y_val = True
+
